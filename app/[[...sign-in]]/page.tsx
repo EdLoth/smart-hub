caf9643a2e3a0df18008c368171d46b886cf5985
EdLoth <eduardosram10@gmail.com>
2024-11-13 14:17:26 -0300

    ✨feat: add Clerk and implement CRUD operations

---
+++ app/[[...sign-in]]/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { SignIn } from '@clerk/nextjs'
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const LoginPage = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const role = user?.publicMetadata.role;
+
+    if (role) {
+      router.push(`/${role}`);
+    }
+  }, [user, router]);
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-lamaSkyLight">
+      <SignIn />
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,16 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+const inter = Inter({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: "Lama Dev School Management Dashboard",
+  description: "Next.js School Management System",
+};
 
 export default function RootLayout({
   children,
@@ -8,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {children} <ToastContainer position="bottom-right" theme="dark" />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
